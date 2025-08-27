03d36db9bb0d07df6aaf8f3ed4566968a97bbe0b
o1ldash123 <adekunbibabayomi@gmail.com>
2025-08-26 18:11:12 -0600

    operators II

---
+++ pythonOperatorsII/andOr.py
@@ -0,0 +1,17 @@
+a = 10
+b = 12
+c =0 
+
+if a and b and c :
+    print('All the numbers have boolean value as true')
+else:
+    print('Atleast one number has boolan value as False')
+
+a = 10
+b = -10
+c = 0
+
+if a > c or b > 0 :
+    print('Either of the number is greater than 0')
+else:
+    print('No number is greater than 0')
